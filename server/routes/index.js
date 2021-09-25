7619d0516ad73d6c31ce68fa4600a5b3380d2105
SilviuDN <silviudilimot@gmail.com>
2021-09-25 19:56:30 +0200

    boilerplate preparation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,3 @@
-const router = require("express").Router();
-
-/* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
-
-module.exports = router;
+module.exports = app => {
+    app.use('/api', require('./base.routes'))
+}
