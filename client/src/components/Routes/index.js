0eb61932ed30a508bade4a83543848cb8c79a759
SilviuDN <silviudilimot@gmail.com>
2021-09-26 15:18:25 +0200

    SignUpForm done

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -1,7 +1,9 @@
 import { Switch, Route } from 'react-router';
+import IndexPage from '../pages/IndexPage/IndexPage';
 import PropertiesPage from '../pages/PropertiesPage/PropertiesPage'
 import PropertyDetails from '../pages/PropertyDetails/PropertyDetails'
 import PropertyForm from '../pages/PropertyForm/PropertyForm';
+import SignUp from '../pages/SignUp/SignUp';
 
 
 const Routes = () => {
@@ -9,9 +11,12 @@ const Routes = () => {
     return(
         <>
             <Switch>
+                <Route path = '/' exact render = { () => <IndexPage/>} />
                 <Route path = '/properties' exact render = { () => <PropertiesPage /> } />
                 <Route path = '/properties/details/:property_id' render = { props => <PropertyDetails {...props}/> } />
                 <Route path = '/properties/new' render = { (props) => <PropertyForm {...props}/> } />
+                <Route path = '/signup' render = { (props) => <SignUp {...props} /> } />
+                {/* <Route path = '/login' render = { (props) => <LogIn {...props} storeUser={storeUser} /> } /> */}
             </Switch>
         </>        
     )
